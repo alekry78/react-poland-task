8cbb4e7c0907512a77a89192665f8be978d6ceea
alekry78 <krysaleksander1@gmail.com>
2021-07-28 23:49:00 +0200

    redux setup and configuration

--- src/components/helpers/ToDo.js
+++ src/components/helpers/ToDo.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import {connect} from "react-redux";
+import {deleteTodo} from "../../actions/addTodo";
 
-const ToDo = () => {
+const ToDo = ({todo,deleteTodo}) => {
+    const{
+        id,
+        name,
+        timestamp,
+        done,
+        description,
+        comments
+    } = todo
     return(
-      <div className="todo">
-
+      <div className="todo" key={id}>
+            <div>
+                <div>
+                    <h1>{name}</h1>
+                    <h3>{description}</h3>
+                </div>
+                <div>
+                    <span>
+                        {timestamp}
+                    </span>
+                    <button></button>
+                    {done ? null : <button>Finish</button>}
+                    <button onClick={() => deleteTodo(id)}>Delete</button>
+                </div>
+            </div>
+          <div>
+              {comments.map(comment=>{
+                  return(
+                      <>
+                          <span>
+                              {comment}
+                          </span>
+                          <button>Delete</button>
+                      </>
+                  )
+              })}
+          </div>
       </div>
     )
 };
-
-export default ToDo
+const mapDispatchToProps = dispatch =>({
+    deleteTodo: key => dispatch(deleteTodo(key))
+})
+export default connect(null,mapDispatchToProps)(ToDo)
